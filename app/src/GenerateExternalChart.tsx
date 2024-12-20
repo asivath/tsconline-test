8fdcb70d25132fedb31bf98516de3193e65c9fce
Changk820 <changk7973@gmail.com>
2024-12-20 05:20:36 -0500

    Added error codes, chart loading, and also disabled certain columns on treatise

--- app/src/GenerateExternalChart.tsx
+++ app/src/GenerateExternalChart.tsx
@@ -1,10 +1,11 @@
-import { useEffect, useContext } from "react";
+import { useEffect, useContext, useState } from "react";
 import { context } from "./state";
 import { getCurrentUserDatapacks, getPublicOfficialDatapacks } from "./state/non-action-util";
 import { ErrorCodes } from "./util/error-codes";
-import { DatapackConfigForChartRequest } from "@tsconline/shared";
+import { ColumnInfo, DatapackConfigForChartRequest } from "@tsconline/shared";
 import { useNavigate, useLocation } from "react-router-dom";
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import LoadingChart from "./LoadingChart";
 
 export function GenerateExternalChart() {
   const location = useLocation();
@@ -12,54 +13,103 @@ export function GenerateExternalChart() {
   const datapackHash = queryParams.get("hash");
   const { state, actions } = useContext(context);
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+  const [errorPushed, setErrorPushed] = useState(false);
+
+  const toggleSpecificColumns = (columnNames: string[]) => {
+    const findAndToggleColumn = (columns: ColumnInfo[], targetName: string) => {
+      for (const column of columns) {
+        if (column.name === targetName) {
+          actions.toggleSettingsTabColumn(column);
+          return true;
+        }
+        if (column.children && column.children.length > 0) {
+          const found = findAndToggleColumn(column.children, targetName);
+          if (found) return true;
+        }
+      }
+      return false;
+    };
+
+    columnNames.forEach((name) => {
+      if (state.settingsTabs.columns && !findAndToggleColumn(state.settingsTabs.columns.children, name)) {
+        console.warn(`Column with name "${name}" not found.`);
+      }
+    });
+  };
 
   useEffect(() => {
+    if (!datapackHash && !errorPushed) {
+      actions.pushError(ErrorCodes.INVALID_DATAPACK_HASH);
+      setErrorPushed(true);
+      return;
+    }
+
     const fetchData = async () => {
       if (datapackHash) {
-        // Call API to load the external datapack
-        await actions.fetchTreatiseDatapacks(datapackHash);
-        // wait 1 seconds for datapack to load
-        await new Promise((resolve) => setTimeout(resolve, 1000));
-        const fetchedDatapack = getCurrentUserDatapacks("treatise", state.datapacks).find(
-          (dp) => dp.title === datapackHash
-        );
-        const internalDatapack = getPublicOfficialDatapacks(state.datapacks).find(
-          (dp) => dp.title === "TimeScale Creator Internal Datapack"
-        );
+        setLoading(true);
+        try {
+          // Call API to load the external datapack
+          await actions.fetchTreatiseDatapacks(datapackHash);
+          // wait 1 seconds for datapack to load
+          await new Promise((resolve) => setTimeout(resolve, 1000));
+          const fetchedDatapack = getCurrentUserDatapacks("treatise", state.datapacks).find(
+            (dp) => dp.title === datapackHash
+          );
+          const internalDatapack = getPublicOfficialDatapacks(state.datapacks).find(
+            (dp) => dp.title === "TimeScale Creator Internal Datapack"
+          );
 
-        if (!internalDatapack) {
-          console.log("Error: Unable to fetch datapack");
-          return;
-        }
-        if (!fetchedDatapack || !internalDatapack) {
+          if (!internalDatapack) {
+            console.log("Error: Unable to fetch datapack");
+            return;
+          }
+          if (!fetchedDatapack || !internalDatapack) {
+            actions.pushError(ErrorCodes.USER_FETCH_DATAPACK_FAILED);
+            return;
+          }
+          const dataReqTreatise: DatapackConfigForChartRequest = {
+            storedFileName: fetchedDatapack.storedFileName,
+            title: fetchedDatapack.title,
+            isPublic: fetchedDatapack.isPublic,
+            uuid: "treatise",
+            type: "user"
+          };
+
+          const internalDatapackConfig: DatapackConfigForChartRequest = {
+            storedFileName: internalDatapack.storedFileName,
+            title: internalDatapack.title,
+            isPublic: internalDatapack.isPublic,
+            type: "official"
+          };
+
+          await actions.processDatapackConfig([internalDatapackConfig, dataReqTreatise]);
+          const columnNamesToToggle = [
+            "Geomagnetic Polarity",
+            "Marine Macrofossils (Mesozoic-Paleozoic)",
+            "Microfossils"
+          ];
+          toggleSpecificColumns(columnNamesToToggle);
+          actions.initiateChartGeneration(navigate, location.pathname);
+        } catch (error) {
           actions.pushError(ErrorCodes.USER_FETCH_DATAPACK_FAILED);
-          return;
+        } finally {
+          setLoading(false);
         }
-        const dataReqTreatise: DatapackConfigForChartRequest = {
-          storedFileName: fetchedDatapack.storedFileName,
-          title: fetchedDatapack.title,
-          isPublic: fetchedDatapack.isPublic,
-          uuid: "treatise",
-          type: "user"
-        };
-
-        const internalDatapackConfig: DatapackConfigForChartRequest = {
-          storedFileName: internalDatapack.storedFileName,
-          title: internalDatapack.title,
-          isPublic: internalDatapack.isPublic,
-          type: "official"
-        };
-
-        await actions.processDatapackConfig([dataReqTreatise, internalDatapackConfig]);
-        actions.initiateChartGeneration(navigate, location.pathname);
       }
     };
     fetchData();
-  }, [datapackHash]);
+  }, [datapackHash, errorPushed]);
 
   if (!datapackHash) {
-    return <Typography>Error: Missing datapack hash</Typography>;
+    return (
+      <Box display="flex" justifyContent="center" alignItems="center" height="100vh" textAlign="center">
+        <Typography variant="h3">Error: Unable to Generate Chart</Typography>
+      </Box>
+    );
   }
 
-  return <Typography>Generating chart for datapack hash: {datapackHash}</Typography>;
+  if (loading) {
+    return <LoadingChart />;
+  }
 }
